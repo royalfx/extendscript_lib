db5797349d921568e1ab95ef326b78199c815337
Alexander Semeniuk <royalfxteam@users.noreply.github.com>
2019-07-23 12:38:38 +0300

    Update as_projectSingleNull.jsxinc

--- lib/project/as_projectSingleNull_usage.jsx
+++ lib/project/as_projectSingleNull_usage.jsx
@@ -1,17 +1,4 @@
-// BATCH
-#includepath "/d/Google Drive/Adobe Scripting/ae_scripts/lib/batch";
-#include "as_loopItemsRecursive.jsxinc";
-#include "as_loopLayers.jsxinc";
-
-// PROJECT
-#includepath "/d/Google Drive/Adobe Scripting/ae_scripts/lib/project";
-#include "as_projectSingleNull.jsxinc";
-#include "as_itemGetType.jsxinc";
-
-// DATA
-#includepath "/d/Google Drive/Adobe Scripting/ae_scripts/lib/data";
-#include "as_arrayCheckValue.jsxinc";
-
+#include "/d/Google Drive/Adobe Scripting/ae_scripts/lib/include.jsxinc";
 
 // UNDO
 app.beginUndoGroup("as_projectSingleNull");
