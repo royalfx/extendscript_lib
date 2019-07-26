100ddf1b94d0e1b349b59fa46b4c47ed58410fe1
Alexander Semeniuk <royalfxteam@users.noreply.github.com>
2019-07-26 14:43:47 +0300

    Update include.jsxinc

---
+++ util/util_updateInclude.jsx
@@ -0,0 +1,12 @@
+#includepath "/d/Google Drive/Adobe Scripting/ae_scripts/lib/file";
+#include "as_singleLibraryJSXINC.jsxinc";
+#include "as_getFileExtension.jsxinc";
+
+#includepath "/d/Google Drive/Adobe Scripting/ae_scripts/lib/loop";
+#include "as_loopDirectories.jsxinc";
+#include "as_loopFiles.jsxinc";
+
+#includepath "/d/Google Drive/Adobe Scripting/ae_scripts/lib/data";
+#include "as_arrayCheckValue.jsxinc";
+
+as_singleLibraryJSXINC(Folder("/d/Google Drive/Adobe Scripting/ae_scripts/lib"));
