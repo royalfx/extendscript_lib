7e0a67bf8b2a31b7a31da69c720f5a28cfc9fc56
Alexander Semeniuk <royalfxteam@users.noreply.github.com>
2019-09-13 20:22:21 +0300

    Removed beginUndoGroup and endUndoGroup calls

--- src/ae/project/as_projectSingleNull_usage.jsx
+++ src/ae/project/as_projectSingleNull_usage.jsx
@@ -1,14 +1,8 @@
 #include "/d/Google Drive/Adobe Scripting/extendscript_lib/src/ae/include.jsxinc";
 
-// UNDO
-app.beginUndoGroup("as_projectSingleNull");
-
 // DO
 if(app.project) {
     as_projectSingleNull();
 } else {
     alert("Open the project!");
 }
-
-// UNDO END
-app.endUndoGroup();
