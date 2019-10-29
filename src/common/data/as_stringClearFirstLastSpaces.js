38e00fe04ae9d98872712bb6e91958c041a254f9
Alexander Semeniuk <royalfxteam@users.noreply.github.com>
2019-10-29 13:08:30 +0200

    Update as_stringClearFirstLastSpaces.js

--- src/common/data/as_stringClearFirstLastSpaces.js
+++ src/common/data/as_stringClearFirstLastSpaces.js
@@ -2,19 +2,23 @@
 // This code is licensed under MIT license
 // See also http://www.opensource.org/licenses/mit-license.php
 
-// version: 1.0.3
-// date: Mon Feb 05 2018
+// version: 1.0.4
+// date: Oct 27 2019
 
 /**
  * 
  * @param {string} str 
  */
 function as_stringClearFirstLastSpaces(str) {
-	while (str.indexOf(" ") == 0) {
+	while (check(str.charAt(0))) {
 		str = str.substr(1);
 	}
-	while (str.substr(-1) == " ") {
+	while (check(str.charAt(str.length - 1))) {
 		str = str.substr(0, str.length - 1);
 	}
 	return str;
+
+	function check(chr) {
+		return (chr === " ") || (chr === "\n") || (chr === "\r") || (chr === "\t");
+	}
 }
