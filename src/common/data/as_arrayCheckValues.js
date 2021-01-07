7ab95e74c23943b0be9d4040aa92defe3d77074f
royalfx <royalfxteam@gmail.com>
2021-01-07 11:30:32 +0200

    Update as_arrayCheckValues.js

--- src/common/data/as_arrayCheckValues.js
+++ src/common/data/as_arrayCheckValues.js
@@ -1,15 +1,21 @@
-// Copyright (c) 2019 Oleksandr Semeniuk
+// Copyright (c) 2021 Oleksandr Semeniuk
 // This code is licensed under MIT license
 // See also http://www.opensource.org/licenses/mit-license.php
 
 /**
- * @version 1.0.0
- * @date Nov 17 2019
+ * @version 1.0.1
+ * @date Jan 07 2021
  * @param {Array} valuesArr 
  * @param {Array} array
  * @returns {number} Number of found values
  */
 function as_arrayCheckValues(valuesArr, array) {
+	if (!(valuesArr instanceof Array)) {
+		throw "An array is expected."
+	}
+	if (!(array instanceof Array)) {
+		throw "An array is expected."
+	}
 	var counter = 0;
 	for (var i = 0, iLen = array.length; i < iLen; i++) {
 		for (var v = 0, vLen = valuesArr.length; v < vLen; v++) {
